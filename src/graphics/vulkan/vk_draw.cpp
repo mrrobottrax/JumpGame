7d8dede500c038f28fd7a0635cd9196f9e27b570
Andrew <mrrobottrax@gmail.com>
2025-03-06 18:23:34 -0500

    Render to small tex

--- src/graphics/vulkan/vk_draw.cpp
+++ src/graphics/vulkan/vk_draw.cpp
@@ -11,6 +11,7 @@
 #include "renderpasses/vk_objects_pass.h"
 #include "game/screen.h"
 #include "vk_renderimage.h"
+#include "vk_queuefamilies.h"
 
 static void DrawSprite(float posX, float posY)
 {
@@ -24,15 +25,8 @@ static void DrawSprite(float posX, float posY)
 	vkCmdDraw(vk_commandbuffer_main, 6, 1, 0, 0);
 }
 
-void DrawFrame(int swapchainImageIndex)
+static void DrawObjectsPass()
 {
-	VkCommandBufferBeginInfo beginInfo{};
-	beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-	beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
-
-	VkAssert(vkResetCommandPool(vk_device, vk_commandpool_main, 0));
-	VkAssert(vkBeginCommandBuffer(vk_commandbuffer_main, &beginInfo));
-
 	VkClearValue clear{
 		.color = {.float32 = {0, 0.5f, 1, 1}}
 	};
@@ -54,29 +48,10 @@ void DrawFrame(int swapchainImageIndex)
 	};
 
 	VkSubpassEndInfo subEnd{
-	.sType = VK_STRUCTURE_TYPE_SUBPASS_END_INFO,
+		.sType = VK_STRUCTURE_TYPE_SUBPASS_END_INFO,
 	};
 
 	vkCmdBeginRenderPass2(vk_commandbuffer_main, &begin, &subBegin);
-
-	// Set viewport
-	VkViewport viewport{
-		.x = 0, .y = 0,
-		.width = (float)game_screen_width,
-		.height = (float)game_screen_height,
-		.minDepth = 0,
-		.maxDepth = 1,
-	};
-	vkCmdSetViewport(vk_commandbuffer_main, 0, 1, &viewport);
-
-	VkRect2D scissor{
-		.offset = { 0, 0 },
-		.extent = {
-			.width = game_screen_width,
-			.height = game_screen_height
-		},
-	};
-	vkCmdSetScissor(vk_commandbuffer_main, 0, 1, &scissor);
 
 	// Set shader
 	vkCmdBindPipeline(vk_commandbuffer_main, VK_PIPELINE_BIND_POINT_GRAPHICS, vk_pipeline);
@@ -85,6 +60,35 @@ void DrawFrame(int swapchainImageIndex)
 	DrawSprite(0, 0);
 
 	vkCmdEndRenderPass2(vk_commandbuffer_main, &subEnd);
+}
+
+static void BlitImage(int swapchainImageIndex)
+{
+	VkImageMemoryBarrier2 toTransferDstBarrier{
+		.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2,
+		.srcStageMask = VK_PIPELINE_STAGE_2_NONE,
+		.srcAccessMask = VK_ACCESS_2_NONE,
+		.dstStageMask = VK_PIPELINE_STAGE_2_BLIT_BIT,
+		.dstAccessMask = VK_ACCESS_2_TRANSFER_WRITE_BIT,
+		.oldLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+		.newLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+		.srcQueueFamilyIndex = vk_queue_family_indices.mainQueueFamily,
+		.dstQueueFamilyIndex = vk_queue_family_indices.mainQueueFamily,
+		.image = vk_swapchain_images[swapchainImageIndex],
+		.subresourceRange = {
+			.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+			.levelCount = 1,
+			.layerCount = 1,
+        },
+	};
+
+	VkDependencyInfo toTransferDstDependency{
+		.sType = VK_STRUCTURE_TYPE_DEPENDENCY_INFO,
+		.imageMemoryBarrierCount = 1,
+		.pImageMemoryBarriers = &toTransferDstBarrier
+	};
+
+	vkCmdPipelineBarrier2(vk_commandbuffer_main, &toTransferDstDependency);
 
 	VkImageBlit2 region{
 		.sType = VK_STRUCTURE_TYPE_IMAGE_BLIT_2,
@@ -120,6 +124,64 @@ void DrawFrame(int swapchainImageIndex)
 	};
 
 	vkCmdBlitImage2(vk_commandbuffer_main, &blitInfo);
+
+	VkImageMemoryBarrier2 toPresentSrcBarrier{
+		.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2,
+		.srcStageMask = VK_PIPELINE_STAGE_2_BLIT_BIT,
+		.srcAccessMask = VK_ACCESS_2_TRANSFER_WRITE_BIT,
+		.dstStageMask = VK_PIPELINE_STAGE_2_NONE,
+		.dstAccessMask = VK_ACCESS_2_NONE,
+		.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+		.newLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,
+		.srcQueueFamilyIndex = vk_queue_family_indices.mainQueueFamily,
+		.dstQueueFamilyIndex = vk_queue_family_indices.mainQueueFamily,
+		.image = vk_swapchain_images[swapchainImageIndex],
+		.subresourceRange = {
+			.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+			.levelCount = 1,
+			.layerCount = 1,
+		},
+	};
+
+	VkDependencyInfo toPresentSrcDependency{
+		.sType = VK_STRUCTURE_TYPE_DEPENDENCY_INFO,
+		.imageMemoryBarrierCount = 1,
+		.pImageMemoryBarriers = &toPresentSrcBarrier
+	};
+
+	vkCmdPipelineBarrier2(vk_commandbuffer_main, &toPresentSrcDependency);
+}
+
+void DrawFrame(int swapchainImageIndex)
+{
+	VkCommandBufferBeginInfo beginInfo{};
+	beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+	beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+	VkAssert(vkResetCommandPool(vk_device, vk_commandpool_main, 0));
+	VkAssert(vkBeginCommandBuffer(vk_commandbuffer_main, &beginInfo));
+
+	// Set viewport
+	VkViewport viewport{
+		.x = 0, .y = 0,
+		.width = (float)game_screen_width,
+		.height = (float)game_screen_height,
+		.minDepth = 0,
+		.maxDepth = 1,
+	};
+	vkCmdSetViewport(vk_commandbuffer_main, 0, 1, &viewport);
+
+	VkRect2D scissor{
+		.offset = { 0, 0 },
+		.extent = {
+			.width = game_screen_width,
+			.height = game_screen_height
+		},
+	};
+	vkCmdSetScissor(vk_commandbuffer_main, 0, 1, &scissor);
+
+	DrawObjectsPass();
+	BlitImage(swapchainImageIndex);
 
 	VkAssert(vkEndCommandBuffer(vk_commandbuffer_main));
 
